25b1d1810af9006a51ab1cc3ecf74a955525decc
anilbey <tuncel.manil@gmail.com>
2019-04-24 11:03:45 +0200

    added argparse to run_secondary_analysis

--- pipeline/secondary_analysis/run_secondary_analysis.py
+++ pipeline/secondary_analysis/run_secondary_analysis.py
@@ -2,8 +2,17 @@ from secondary_analysis import SecondaryAnalysis
 import argparse
 
 
-# parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser()
+parser.add_argument("-s", "--sample_name", required=True)
+parser.add_argument("-o","--output_path", required=True)
+parser.add_argument("-h5","--h5_path", required=True, help="path to the input h5")
+parser.add_argument("-g","--genes_path", required=True, help="path to the genes of interest and their coordinates")
+parser.add_argument("-b","--bins_to_remove", required=True, help="10x genomics technology artifact bins")
 
-sa = SecondaryAnalysis(sample_name='MATIWAQ-T_scD_Ar1v1.3', output_path='MATIWAQ', h5_path='..//test_dir/cnv_data.h5', genes_path='../../required_files/genes_of_interest/melanoma_gene_coordinates.tsv')
-sa.remove_tenx_genomics_artifacts(bins='/Users/mtuncel/git_repos/dna-pipeline/required_files/10x_artifacts/GRCh37_10x_artifacts_mask_whole_genome.tsv')
+
+args = parser.parse_args()
+
+sa = SecondaryAnalysis(sample_name=args.sample_name, output_path=args.output_path, h5_path=args.h5_path, genes_path=args.genes_path)
+sa.remove_tenx_genomics_artifacts(bins=args.bins_to_remove)
 sa.apply_phenograph()
+
