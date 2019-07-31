195ca8e3d4006e41304cc5956d36a9d7f400640a
Mustafa Anil Tuncel <tuncel.manil@gmail.com>
2019-07-31 15:51:15 +0200

    [code style] scripts/ PAINT IT BLACK

--- scripts/cellranger_dna_trick.py
+++ scripts/cellranger_dna_trick.py
@@ -1,4 +1,3 @@
-
 # coding: utf-8
 
 # ### Tricking CellrangerDNA with Less base pairs than supported
@@ -14,9 +13,9 @@ import argparse
 import gzip
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-r1","--read1",required=True,help="read 1 fastq")
-parser.add_argument("-r2","--read2",required=True,help="read 2 fastq")
-parser.add_argument("-o","--output",required=True,help="output fastqs path")
+parser.add_argument("-r1", "--read1", required=True, help="read 1 fastq")
+parser.add_argument("-r2", "--read2", required=True, help="read 2 fastq")
+parser.add_argument("-o", "--output", required=True, help="output fastqs path")
 
 args = parser.parse_args()
 
@@ -28,29 +27,27 @@ r2_fastq = args.read2
 # R1 fastq file
 r1_fastq = args.read1
 # new fastq path
-new_fastq_path = args.output + '/'
+new_fastq_path = args.output + "/"
 
 
-with gzip.open(new_fastq_path+r1_fastq.split('/')[-1],"wb") as new_r1_fastq:
+with gzip.open(new_fastq_path + r1_fastq.split("/")[-1], "wb") as new_r1_fastq:
 
-    with gzip.open(r1_fastq, "rt") as r1, gzip.open(r2_fastq, "rt") as r2: 
+    with gzip.open(r1_fastq, "rt") as r1, gzip.open(r2_fastq, "rt") as r2:
         i = 0
-        read1 = ''
-        read2 = ''
+        read1 = ""
+        read2 = ""
         for x, y in tqdm(zip(r1, r2)):
-            
+
             read1 = x.strip()
             read2 = y.strip()
-            #import ipdb; ipdb.set_trace() # debugging starts here
-            if i%2 == 1: # trim read1 by 16
+            # import ipdb; ipdb.set_trace() # debugging starts here
+            if i % 2 == 1:  # trim read1 by 16
                 read1 = read1[0:16]
-                if i%4 == 1: # the sequence
+                if i % 4 == 1:  # the sequence
                     read2_seq = Seq(read2)
                     read1 += str(read2_seq.reverse_complement())[0:42]
-                if i%4 == 3:
+                if i % 4 == 3:
                     read1 += read2[::-1][0:42]
             new_r1_fastq.write(read1.encode())
-            new_r1_fastq.write('\n'.encode())
+            new_r1_fastq.write("\n".encode())
             i += 1
-
-
