28f0159466f1df66937b2afcc0585b94f1d3c984
pedrofale <pedro.ferreira@bsse.ethz.ch>
2019-11-04 11:37:46 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="dnapipe",
+    version='1.0dev',
+    description='Workflow for single-cell CNV data analysis',
+    author='Mustafa Anil Tuncel',
+    author_email='tuncel.manil@gmail.com',
+    packages=find_packages(),
+    install_requires=requirements
+)
