8fc41b4cd63ccf30cae71659910cfe253d19e779
Monica Dragan <mdragan@eu-login-12.euler.ethz.ch>
2020-02-07 14:44:42 +0100

    fix run_dna_pipeline.sh

--- scripts/run_dna_pipeline.sh
+++ scripts/run_dna_pipeline.sh
@@ -16,6 +16,6 @@ fi
 module load python/3.6.0 graphviz hdf5/1.8.12 gcc/6.2.0 eth_proxy
 module load /cluster/work/bewi/modules/cellranger-dna/1.1.0/cellranger-dna-1.1.0/cellranger-dna.modulefile
 
-out_path="/cluster/work/bewi/ngs/projects/tumorProfiler/analysis/trial_${2}/${1}-T/singlecell_dna/snake_analysis_files/" 
-config_path="${out_path}config.json"
-bsub -J ${1} -n 48 -W 23:57 -R fullnode -R "rusage[mem=5000]" snakemake -s /cluster/work/bewi/ngs/projects/tumorProfiler/code/dna-pipeline-novaseq/pipeline/Snakefile --configfile ${config_path} -j 48 -p -k -o ${out_path}
+cd /cluster/work/bewi/ngs/projects/tumorProfiler/analysis/trial_${2}/${1}-T/singlecell_dna/snake_analysis_files/
+
+bsub -J ${1} -n 48 -W 23:57 -R fullnode -R "rusage[mem=5000]" snakemake -s /cluster/work/bewi/ngs/projects/tumorProfiler/code/dna-pipeline-novaseq/pipeline/Snakefile --configfile ./config.json -j 48 -p -k 
