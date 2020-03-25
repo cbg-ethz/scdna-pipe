c4998871c05516857e1014eb4ae8887fa773de96
Pedro Ferreira <pedro.ferreira@bsse.ethz.ch>
2020-03-26 00:23:26 +0100

    Update assess_cell_fusions.py

--- scripts/assess_cell_fusions.py
+++ scripts/assess_cell_fusions.py
@@ -43,7 +43,7 @@ parser.add_argument("-o", "--output_file", required=True, help="Output file")
 
 args = parser.parse_args()
 
-input_tree = args.input_tree
+input_tree_file = args.input_tree
 input_segmented_data = args.input_segmented_data
 input_segmented_region_sizes = args.input_segmented_region_sizes
 region_neutral_states_file = args.region_neutral_states_file
@@ -56,23 +56,25 @@ segmented_data = np.loadtxt(input_segmented_data, delimiter=",")
 segmented_region_sizes = np.loadtxt(input_segmented_region_sizes, delimiter=",")
 region_neutral_states = np.loadtxt(region_neutral_states_file, delimiter=",")
 
-learned_tree = Tree(bin, "")
-learned_tree.read_tree_str(input_tree)
+learned_tree = Tree(bin, "", persistence=True)
+learned_tree.read_from_file(input_tree_file)
 fusion_tree = create_fusion_tree(learned_tree, region_neutral_states)
+
 fusion_tree.learn_tree(
     segmented_data,
     segmented_region_sizes,
-    n_iters=1,
+    n_iters=0,
     move_probs=[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.00],
     seed=42,
     postfix="fusion_tree",
     initial_tree=fusion_tree,
     region_neutral_states=region_neutral_states,
     copy_number_limit=10,
+    verbosity=2
 )
 
 unique_assignments, unique_assignments_idx, tree_cluster_sizes = np.unique(
-    fusion_tree.outputs["cell_node_ids"], return_index=True, return_counts=True
+    fusion_tree.outputs["cell_node_ids"][:,1], return_index=True, return_counts=True
 )
 
 # Map node ID to subclone ID
@@ -99,19 +101,25 @@ for c_id in range(unique_cnvs.shape[0]):
 # Map node IDs to labels
 _, idx = np.unique(labels, return_index=True)
 original_cell_node_ids = np.loadtxt(original_cell_node_ids_file, delimiter=',')
-node_label_map = dict(zip(original_cell_node_ids[idx].astype(str), labels[idx]))
+node_label_map = dict(zip(original_cell_node_ids[idx].astype(int).astype(str), labels[idx].astype(int).astype(str)))
 
 # Now add entries for the fusion nodes to the map by prefixing with 'F'
 for node in list(node_label_map):
-    node_label_map[str(int(node_label_map[node]) + 1000)] = "F" + node
+    node_label_map[str(int(float(node) + 1000))] = "F" + str(int(float(node_label_map[node])))
 
 n_cells = inferred_cnvs.shape[0]
-new_cell_node_ids = np.empty(n_cells)
+new_cell_node_ids = (np.zeros(n_cells)-1).astype(str)
+
+print(node_label_map)
+print(np.unique(fusion_tree.outputs['cell_node_ids'][:,1]).astype(str))
 # Replace cell_node_ids from fusion tree with new labels
-fusion_cell_node_ids = fusion_tree.outputs['cell_node_ids']
+fusion_cell_node_ids = fusion_tree.outputs['cell_node_ids'][:,1]
 for node_id in np.unique(fusion_cell_node_ids):
-    cells = np.where(np.all(fusion_cell_node_ids == node_id, axis=1))[0]
-    new_cell_node_ids[cells] == node_label_map[str(int(node_id))]
+    cells = np.where(fusion_cell_node_ids == node_id)[0]
+    try:
+        new_cell_node_ids[cells] = node_label_map[str(int(float(node_id)))]
+    except:
+        pass
 
 # Count number of cells attached to each node
 uniques, counts = np.unique(new_cell_node_ids, return_counts=True)
@@ -123,9 +131,9 @@ with open(output_file, "w") as text_file:
         file=text_file,
     )
     print(
-        "Fusion clusters are labeled 'F\{subclone_id\}',"
-        " where \{subclone_id\} indicates the ID of the subclones whose CNV profiles"
-        " are plotted in the output PNG file \{sample_prefix\}__cluster_profile_overlapping.",
+        "Fusion clusters are labeled 'F{subclone_id}',"
+        " where {subclone_id} indicates the ID of the subclones whose CNV profiles"
+        " are plotted in the output PNG file {sample_prefix}__cluster_profile_overlapping.",
         file=text_file,
     )
     print(
@@ -135,10 +143,13 @@ with open(output_file, "w") as text_file:
     )
     fcounts = 0
     for i, unique in enumerate(uniques):
-        if unique.contains("F"):
+        if "F" in unique:
             fcounts += counts[i]
-            print(f"{unique}: {counts[i]}", file=text_file)
-    print("", file=text_file)  # end with empty newline
+            print(
+                f"{unique}: {counts[i]}",
+                file=text_file,
+            )
     print(
-        f"In total, there are {fcounts} possible doublets/fusion cells, out of {n_cells} ({fcounts/n_cells})."
+        f"In total, there are {fcounts} possible doublets/fusion cells, out of {n_cells} ({fcounts/n_cells}).",
+        file=text_file,
     )
